279e34588fb463e68fa332fe0003e9c2d8d1b34b
Tayná Tinôco <a10729@alunos.ipca.pt>
2016-12-02 12:36:04 +0000

    Drawable

--- app/src/main/java/taynact/projetocm/MainActivity.java
+++ app/src/main/java/taynact/projetocm/MainActivity.java
@@ -1,15 +1,21 @@
 package taynact.projetocm;
 
+import android.graphics.Bitmap;
+import android.support.v4.media.MediaMetadataCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Bitmap img = Bitmap.createBitmap(@dra)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
 //HELLO WORLD
 //Hello
+
